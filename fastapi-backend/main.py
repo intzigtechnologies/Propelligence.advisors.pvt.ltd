0d11b8caa4d5d4b5817c4e41e8bad7ae13200ae8
intzigtechnologies <intzigtechnologies@gmail.com>
2025-10-26 22:44:41 +0530

    added some code to backend - added rich library to stylize a little

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
+from rich.console import Console
+from rich.panel import Panel
 from log.logging_config import async_log
 from contextlib import asynccontextmanager
 from middleware.log_middleware import LogRequestsMiddleware
 
+console = Console()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # Startup
+    console.print(Panel("[bold green]🚀 FastAPI app started successfully![/bold green]", expand=False))
     await async_log("info", "🚀 FastAPI app started successfully")
-    yield  # <-- app runs during this period
+
+    yield  #app running during this 
+
+    # Shutdown
+    console.print(Panel("[bold red]🛑 FastAPI app shutting down[/bold red]", expand=False))
     await async_log("info", "🛑 FastAPI app shutting down")
+
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(LogRequestsMiddleware)
@@ -22,9 +32,5 @@ async def root():
     
     
 
-    
-    
-    
-    
     
 
